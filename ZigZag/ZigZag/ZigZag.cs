01e85a9639af8f1fb65ac8cd98e747fc938a6fee
Tamegoro <tamemobile@gmail.com>
2013-08-04 16:41:30 +0900

    Add "ZigZag".

---
+++ ZigZag/ZigZag/ZigZag.cs
@@ -0,0 +1,234 @@
+﻿using System;
+using Microsoft.SPOT;
+using Microsoft.SPOT.Hardware;
+using Microsoft.SPOT.Presentation;
+using Microsoft.SPOT.Presentation.Media;
+using System.Threading;
+using AGENT.AZMutil.AZMDrawing;
+using Agent.Contrib.Hardware;
+
+namespace ZigZag
+{
+    public class ZigZag
+    {
+
+
+        static Bitmap _display;
+
+        static Timer _updateClockTimer;
+        static TimeSpan dueTime;
+        static TimeSpan period;
+
+        static DateTime currentTime;
+
+        static Font font7barPBd24 = Resources.GetFont(Resources.FontResources._7barPBd24);
+
+        static Color colorForeground;
+        static Color colorBackground;
+
+        static AZMDrawing _azmdrawing;
+        static AGENT.AZMutil.Point _point;
+
+        static int degreeH = 0;
+        static int degreeM = 0;
+        static int degreeS = 0;
+
+        static int screenWidth = 0;
+        static int screenHeight = 0;
+
+        static int screenCenterX = 0;
+        static int screenCenterY = 0;
+
+        static int displayMode = DISPLAY_MODE_WHITE;
+
+        static bool showDigital = false;
+        static int showDigitalCounter = 0;
+
+        const int MAX_DISPLAY_MODE = 1;
+
+        const int SHOW_DIGITAL_SECOND = 10;
+
+
+        const int LENGTH_HOUR_HAND = 20;
+        const int LENGTH_MINUTE_HAND = 20;
+        const int LENGTH_SECOND_HAND = 20;
+
+        const int DISPLAY_MODE_WHITE = 0;
+        const int DISPLAY_MODE_BLACK = 1;
+
+
+
+        public static void Main()
+        {
+
+            _display = new Bitmap(Bitmap.MaxWidth, Bitmap.MaxHeight);
+
+            _azmdrawing = new AZMDrawing();
+            _point = new AGENT.AZMutil.Point();
+
+            screenWidth = _display.Width;
+            screenHeight = _display.Height;
+
+            screenCenterX = screenWidth / 2;
+            screenCenterY = screenHeight / 2;
+
+            displayMode = DISPLAY_MODE_WHITE;
+            colorForeground = Color.Black;
+            colorBackground = Color.White;
+
+            showDigital = false;
+
+            currentTime = new DateTime();
+            currentTime = DateTime.Now;
+
+            UpdateTime(null);
+
+            currentTime = DateTime.Now;
+
+            dueTime = new TimeSpan(0, 0, 0, 0, 1000 - currentTime.Millisecond);
+            period = new TimeSpan(0, 0, 0, 1, 0);
+
+            _updateClockTimer = new Timer(UpdateTime, null, dueTime, period);
+
+            ButtonHelper.ButtonSetup = new Buttons[]
+            {
+                Buttons.BottomRight, Buttons.MiddleRight, Buttons.TopRight
+            };
+
+            ButtonHelper.Current.OnButtonPress += Current_OnButtonPress;
+
+            Thread.Sleep(Timeout.Infinite);
+
+        }
+
+        static void UpdateTime(object state)
+        {
+
+            currentTime = DateTime.Now;
+
+            _display.Clear();
+
+
+            if (showDigital == false)
+            {
+
+                degreeH = _azmdrawing.HourToAngle(currentTime.Hour, currentTime.Minute);
+                degreeM = _azmdrawing.MinuteToAngle(currentTime.Minute);
+                degreeS = _azmdrawing.SecondToAngle(currentTime.Second);
+
+                _display.DrawRectangle(colorBackground, 1, 0, 0, screenWidth, screenHeight, 0, 0, colorBackground, 0, 0, colorBackground, 0, 0, 255);
+
+                _point.X = screenCenterX;
+                _point.Y = screenCenterY;
+                _azmdrawing.DrawAngledLine(_display, colorForeground, 2, degreeH, _point.X, _point.Y, 0, LENGTH_HOUR_HAND, 0);
+                _display.DrawEllipse(colorForeground, 1, _point.X, _point.Y, 2, 2, colorBackground, 0, 0, colorBackground, 0, 0, 255);
+                _display.DrawEllipse(colorBackground, 1, _point.X, _point.Y, 1, 1, colorForeground, 0, 0, colorForeground, 0, 0, 255);
+
+                _point = _azmdrawing.FindPointDegreeDistance(degreeH, _point.X, _point.Y, LENGTH_HOUR_HAND);
+                _azmdrawing.DrawAngledLine(_display, colorForeground, 2, degreeM, _point.X, _point.Y, 0, LENGTH_MINUTE_HAND, 0);
+                _display.DrawEllipse(colorForeground, 1, _point.X, _point.Y, 2, 2, colorBackground, 0, 0, colorBackground, 0, 0, 255);
+                _display.DrawEllipse(colorBackground, 1, _point.X, _point.Y, 1, 1, colorForeground, 0, 0, colorForeground, 0, 0, 255);
+
+                _point = _azmdrawing.FindPointDegreeDistance(degreeM, _point.X, _point.Y, LENGTH_MINUTE_HAND);
+                _azmdrawing.DrawAngledLine(_display, colorForeground, 2, degreeS, _point.X, _point.Y, 0, LENGTH_SECOND_HAND, 1);
+                _display.DrawEllipse(colorForeground, 1, _point.X, _point.Y, 2, 2, colorBackground, 0, 0, colorBackground, 0, 0, 255);
+                _display.DrawEllipse(colorBackground, 1, _point.X, _point.Y, 1, 1, colorForeground, 0, 0, colorForeground, 0, 0, 255);
+
+            }
+            else
+            {
+
+                _azmdrawing.DrawDigitalClock(_display, Color.White, Color.Black, font7barPBd24, currentTime, true);
+                ++showDigitalCounter;
+
+                if (showDigitalCounter > 10)
+                {
+                    showDigital = false;
+                    showDigitalCounter = 0;
+                }
+            
+            }
+
+            _display.Flush();
+        
+        }
+
+        private static void Current_OnButtonPress(Buttons button, Microsoft.SPOT.Hardware.InterruptPort port, ButtonDirection direction, DateTime time)
+        {
+
+            if (direction == ButtonDirection.Up)
+            {
+                if (button == Buttons.TopRight)
+                {
+                    if (showDigital == false)
+                    {
+                        --displayMode;
+                        if (displayMode < 0)
+                        {
+                            displayMode = MAX_DISPLAY_MODE;
+                        }
+                    }
+                    else
+                    {
+                        showDigital = false;
+                    }
+                }
+                else if (button == Buttons.MiddleRight)
+                {
+                    if (showDigital != true)
+                    {
+                        showDigital = true;
+                        showDigitalCounter = 0;
+                        UpdateTime(null);
+                    }
+                    else
+                    {
+                        showDigital = false;
+                    }
+                }
+                else if (button == Buttons.BottomRight)
+                {
+                    if (showDigital == false)
+                    {
+                        ++displayMode;
+                        if (displayMode > MAX_DISPLAY_MODE)
+                        {
+                            displayMode = 0;
+                        }
+                    }
+                    else
+                    {
+                        showDigital = false;
+                    }
+                }
+
+                switch (displayMode)
+                {
+
+                    case DISPLAY_MODE_WHITE:
+
+                        colorForeground = Color.Black;
+                        colorBackground = Color.White;
+                        UpdateTime(null);
+
+                        break;
+
+                    case DISPLAY_MODE_BLACK:
+
+                        colorForeground = Color.White;
+                        colorBackground = Color.Black;
+                        UpdateTime(null);
+
+                        break;
+
+                }
+
+            }
+
+        }
+
+
+    }
+
+}
+
