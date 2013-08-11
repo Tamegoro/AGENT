f711e1a48684e8503f706b603c1a68cca1398a67
Tamegoro <tamemobile@gmail.com>
2013-08-11 18:50:08 +0900

    Add "Circles".

---
+++ Circles/Circles/Circles.cs
@@ -0,0 +1,255 @@
+﻿using System;
+using Microsoft.SPOT;
+using Microsoft.SPOT.Hardware;
+using Microsoft.SPOT.Presentation;
+using Microsoft.SPOT.Presentation.Media;
+using System.Threading;
+using AGENT.AZMutil.AZMDrawing;
+using Agent.Contrib.Hardware;
+
+
+namespace Circles
+{
+    public class Circles
+    {
+
+        static Bitmap _display;
+
+        static Timer _updateClockTimer;
+        static TimeSpan dueTime;
+        static TimeSpan period;
+
+        static DateTime currentTime;
+
+        static Font font7barPBd24 = Resources.GetFont(Resources.FontResources._7barPBd24);
+
+        static Color colorForeground;
+        static Color colorBackground;
+
+        static AZMDrawing _azmdrawing;
+        static AGENT.AZMutil.Point _point;
+
+        static int degreeH = 0;
+        static int degreeM = 0;
+        static int degreeS = 0;
+
+        static int screenWidth = 0;
+        static int screenHeight = 0;
+
+        static int screenCenterX = 0;
+        static int screenCenterY = 0;
+
+        static int displayMode = DISPLAY_MODE_BLACK;
+
+        static bool showDial = false;
+
+        static bool showDigital = false;
+        static int showDigitalCounter = 0;
+
+        const int SHOW_DIGITAL_SECOND = 10;
+
+        const int THIKNESS_CIRCLE = 4;
+
+        const int MARGIN_CIRCLE_EDGE = 3;
+        const int MARGIN_CIRCLE_CIRCLE = 5;
+
+        const int MAX_DISPLAY_MODE = 1;
+
+        const int DISPLAY_MODE_BLACK = 0;
+        const int DISPLAY_MODE_BLACK_DIAL = 1;
+
+
+        public static void Main()
+        {
+
+            _display = new Bitmap(Bitmap.MaxWidth, Bitmap.MaxHeight);
+
+            _azmdrawing = new AZMDrawing();
+            _point = new AGENT.AZMutil.Point();
+
+            screenWidth = _display.Width;
+            screenHeight = _display.Height;
+
+            screenCenterX = screenWidth / 2;
+            screenCenterY = screenHeight / 2;
+
+            displayMode = DISPLAY_MODE_BLACK;
+            showDial = false;
+            colorForeground = Color.White;
+            colorBackground = Color.Black;
+
+            showDigital = false;
+
+            currentTime = new DateTime();
+            currentTime = DateTime.Now;
+
+            UpdateTime(null);
+
+            currentTime = DateTime.Now;
+
+            dueTime = new TimeSpan(0, 0, 0, 0, 1000 - currentTime.Millisecond);
+            period = new TimeSpan(0, 0, 0, 1, 0);
+
+            _updateClockTimer = new Timer(UpdateTime, null, dueTime, period);
+
+            ButtonHelper.ButtonSetup = new Buttons[]
+            {
+                Buttons.BottomRight, Buttons.MiddleRight, Buttons.TopRight
+            };
+
+            ButtonHelper.Current.OnButtonPress += Current_OnButtonPress;
+
+            Thread.Sleep(Timeout.Infinite);
+
+        }
+
+        static void UpdateTime(object state)
+        {
+
+            currentTime = DateTime.Now;
+
+            _display.Clear();
+
+
+            if (showDigital == false)
+            {
+
+                degreeH = _azmdrawing.HourToAngle(currentTime.Hour, currentTime.Minute);
+                degreeM = _azmdrawing.MinuteToAngle(currentTime.Minute);
+                degreeS = _azmdrawing.SecondToAngle(currentTime.Second);
+
+                _display.DrawRectangle(colorBackground, 1, 0, 0, screenWidth, screenHeight, 0, 0, colorBackground, 0, 0, colorBackground, 0, 0, 255);
+
+
+                for (int i = 0; i <= 2; i++)
+                {
+                    _display.DrawEllipse(colorForeground, 1, screenCenterX, screenCenterY, screenCenterX - MARGIN_CIRCLE_EDGE - ((THIKNESS_CIRCLE + MARGIN_CIRCLE_CIRCLE) * i), screenCenterY - MARGIN_CIRCLE_EDGE - ((THIKNESS_CIRCLE + MARGIN_CIRCLE_CIRCLE) * i), colorForeground, 0, 0, colorForeground, 0, 0, 255);
+                    _display.DrawEllipse(colorBackground, 1, screenCenterX, screenCenterY, screenCenterX - MARGIN_CIRCLE_EDGE - ((THIKNESS_CIRCLE + MARGIN_CIRCLE_CIRCLE) * i) - THIKNESS_CIRCLE, screenCenterY - MARGIN_CIRCLE_EDGE - ((THIKNESS_CIRCLE + MARGIN_CIRCLE_CIRCLE) * i) - THIKNESS_CIRCLE, colorBackground, 0, 0, colorBackground, 0, 0, 255);
+                }
+
+                _azmdrawing.DrawAngledLine(_display, colorBackground, 1, 0, screenCenterX, screenCenterY, screenCenterX - MARGIN_CIRCLE_EDGE - ((THIKNESS_CIRCLE + MARGIN_CIRCLE_CIRCLE) * 2) - THIKNESS_CIRCLE - 2, ((THIKNESS_CIRCLE + MARGIN_CIRCLE_CIRCLE) * 3) + 4);
+                _azmdrawing.DrawAngledLine(_display, colorBackground, 1, degreeS, screenCenterX, screenCenterY, screenCenterX - MARGIN_CIRCLE_EDGE - ((THIKNESS_CIRCLE + MARGIN_CIRCLE_CIRCLE) * 0) - THIKNESS_CIRCLE - 2, THIKNESS_CIRCLE + 4);
+                _azmdrawing.DrawAngledLine(_display, colorBackground, 1, degreeM, screenCenterX, screenCenterY, screenCenterX - MARGIN_CIRCLE_EDGE - ((THIKNESS_CIRCLE + MARGIN_CIRCLE_CIRCLE) * 1) - THIKNESS_CIRCLE - 2, THIKNESS_CIRCLE + 4);
+                _azmdrawing.DrawAngledLine(_display, colorBackground, 1, degreeH, screenCenterX, screenCenterY, screenCenterX - MARGIN_CIRCLE_EDGE - ((THIKNESS_CIRCLE + MARGIN_CIRCLE_CIRCLE) * 2) - THIKNESS_CIRCLE - 2, THIKNESS_CIRCLE + 4);
+
+                _point = _azmdrawing.FindPointDegreeDistance(0, screenCenterX, screenCenterY, screenCenterX - MARGIN_CIRCLE_EDGE - ((THIKNESS_CIRCLE + MARGIN_CIRCLE_CIRCLE) * 0) - (THIKNESS_CIRCLE / 2));
+                _point = _azmdrawing.FindPointDegreeDistance(0 - 90, _point.X, _point.Y, 1);
+                _azmdrawing.FillArea(_display, colorBackground, _point.X, _point.Y);
+
+                _point = _azmdrawing.FindPointDegreeDistance(0, screenCenterX, screenCenterY, screenCenterX - MARGIN_CIRCLE_EDGE - ((THIKNESS_CIRCLE + MARGIN_CIRCLE_CIRCLE) * 1) - (THIKNESS_CIRCLE / 2));
+                _point = _azmdrawing.FindPointDegreeDistance(0 - 90, _point.X, _point.Y, 1);
+                _azmdrawing.FillArea(_display, colorBackground, _point.X, _point.Y);
+
+                _point = _azmdrawing.FindPointDegreeDistance(0, screenCenterX, screenCenterY, screenCenterX - MARGIN_CIRCLE_EDGE - ((THIKNESS_CIRCLE + MARGIN_CIRCLE_CIRCLE) * 2) - (THIKNESS_CIRCLE / 2));
+                _point = _azmdrawing.FindPointDegreeDistance(0 - 90, _point.X, _point.Y, 1);
+                _azmdrawing.FillArea(_display, colorBackground, _point.X, _point.Y);
+
+                if (showDial == true)
+                {
+                    for (int i = 1; i < 12; i++)
+                    {
+                        _azmdrawing.DrawAngledLine(_display, colorBackground, 2, 30 * i, screenCenterX, screenCenterY, screenCenterX - MARGIN_CIRCLE_EDGE - ((THIKNESS_CIRCLE + MARGIN_CIRCLE_CIRCLE) * 0) - THIKNESS_CIRCLE - 1, THIKNESS_CIRCLE + 2);
+                        _azmdrawing.DrawAngledLine(_display, colorBackground, 2, 30 * i, screenCenterX, screenCenterY, screenCenterX - MARGIN_CIRCLE_EDGE - ((THIKNESS_CIRCLE + MARGIN_CIRCLE_CIRCLE) * 1) - THIKNESS_CIRCLE - 1, THIKNESS_CIRCLE + 2);
+                        _azmdrawing.DrawAngledLine(_display, colorBackground, 2, 30 * i, screenCenterX, screenCenterY, screenCenterX - MARGIN_CIRCLE_EDGE - ((THIKNESS_CIRCLE + MARGIN_CIRCLE_CIRCLE) * 2) - THIKNESS_CIRCLE - 1, THIKNESS_CIRCLE + 2);
+                    }
+                }
+
+            }
+            else
+            {
+
+                _azmdrawing.DrawDigitalClock(_display, Color.White, Color.Black, font7barPBd24, currentTime, true);
+                ++showDigitalCounter;
+
+                if (showDigitalCounter > SHOW_DIGITAL_SECOND)
+                {
+                    showDigital = false;
+                    showDigitalCounter = 0;
+                }
+
+            }
+
+            _display.Flush();
+
+        }
+
+        private static void Current_OnButtonPress(Buttons button, Microsoft.SPOT.Hardware.InterruptPort port, ButtonDirection direction, DateTime time)
+        {
+
+            if (direction == ButtonDirection.Up)
+            {
+                if (button == Buttons.TopRight)
+                {
+                    if (showDigital == false)
+                    {
+                        --displayMode;
+                        if (displayMode < 0)
+                        {
+                            displayMode = MAX_DISPLAY_MODE;
+                        }
+                    }
+                    else
+                    {
+                        showDigital = false;
+                    }
+                }
+                else if (button == Buttons.MiddleRight)
+                {
+                    if (showDigital != true)
+                    {
+                        showDigital = true;
+                        showDigitalCounter = 0;
+                    }
+                    else
+                    {
+                        showDigital = false;
+                    }
+                }
+                else if (button == Buttons.BottomRight)
+                {
+                    if (showDigital == false)
+                    {
+                        ++displayMode;
+                        if (displayMode > MAX_DISPLAY_MODE)
+                        {
+                            displayMode = 0;
+                        }
+                    }
+                    else
+                    {
+                        showDigital = false;
+                    }
+                }
+
+                switch (displayMode)
+                {
+
+                    case DISPLAY_MODE_BLACK:
+
+                        showDial = false;
+                        colorForeground = Color.White;
+                        colorBackground = Color.Black;
+                        UpdateTime(null);
+
+                        break;
+
+                    case DISPLAY_MODE_BLACK_DIAL:
+
+                        showDial = true;
+                        colorForeground = Color.White;
+                        colorBackground = Color.Black;
+                        UpdateTime(null);
+
+                        break;
+
+                }
+
+            }
+
+        }
+
+
+    }
+
+}
+
