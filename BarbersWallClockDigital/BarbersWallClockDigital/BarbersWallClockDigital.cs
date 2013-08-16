893f55938d2fa9e24158f799a95f2fa3586dcdb9
Tamegoro <tamemobile@gmail.com>
2013-08-16 16:05:23 +0900

    Add "Barber's Wall Clock (Digital)".

---
+++ BarbersWallClockDigital/BarbersWallClockDigital/BarbersWallClockDigital.cs
@@ -0,0 +1,213 @@
+﻿using System;
+using Microsoft.SPOT;
+using Microsoft.SPOT.Hardware;
+using Microsoft.SPOT.Presentation;
+using Microsoft.SPOT.Presentation.Media;
+using System.Threading;
+using AGENT.AZMutil.AZMDrawing;
+using Agent.Contrib.Hardware;
+
+
+namespace BarbersWallClockDigital
+{
+    public class BarbersWallClockDigital
+    {
+
+        static Bitmap _display;
+
+        static Timer _updateClockTimer;
+        static TimeSpan dueTime;
+        static TimeSpan period;
+
+        static DateTime currentTime;
+
+        static Font font_7barPBd24 = Resources.GetFont(Resources.FontResources._7barPBd24);
+        static Font font_7barPBdReverse24 = Resources.GetFont(Resources.FontResources._7barPBdReverse24);
+
+        static Color colorForeground;
+        static Color colorBackground;
+
+        static AZMDrawing _azmdrawing;
+
+        static string stringH = "";
+        static string stringM = "";
+        static string stringS = "";
+
+        static int screenWidth = 0;
+        static int screenHeight = 0;
+
+        static int screenCenterX = 0;
+        static int screenCenterY = 0;
+
+        static int displayMode = DISPLAY_MODE_BLACK;
+
+        static bool showDigital = false;
+        static int showDigitalCounter = 0;
+
+
+        const int SHOW_DIGITAL_SECOND = 10;
+
+
+        const int MAX_DISPLAY_MODE = 1;
+
+        const int DISPLAY_MODE_BLACK = 0;
+        const int DISPLAY_MODE_WHITE = 1;
+
+
+        public static void Main()
+        {
+
+            _display = new Bitmap(Bitmap.MaxWidth, Bitmap.MaxHeight);
+
+            _azmdrawing = new AZMDrawing();
+
+            screenWidth = _display.Width;
+            screenHeight = _display.Height;
+
+            screenCenterX = screenWidth / 2;
+            screenCenterY = screenHeight / 2;
+
+            displayMode = DISPLAY_MODE_BLACK;
+            colorForeground = Color.White;
+            colorBackground = Color.Black;
+
+            showDigital = false;
+
+            currentTime = new DateTime();
+            currentTime = DateTime.Now;
+
+            UpdateTime(null);
+
+            currentTime = DateTime.Now;
+
+            dueTime = new TimeSpan(0, 0, 0, 0, 1000 - currentTime.Millisecond);
+            period = new TimeSpan(0, 0, 0, 1, 0);
+
+            _updateClockTimer = new Timer(UpdateTime, null, dueTime, period);
+
+            ButtonHelper.ButtonSetup = new Buttons[]
+            {
+                Buttons.BottomRight, Buttons.MiddleRight, Buttons.TopRight
+            };
+
+            ButtonHelper.Current.OnButtonPress += Current_OnButtonPress;
+
+            Thread.Sleep(Timeout.Infinite);
+
+        }
+
+        static void UpdateTime(object state)
+        {
+
+            currentTime = DateTime.Now;
+
+            _display.Clear();
+
+            if (showDigital == false)
+            {
+
+                stringH = (currentTime.Hour % 10).ToString() + (currentTime.Hour / 10).ToString();
+                stringM = (currentTime.Minute % 10).ToString() + (currentTime.Minute / 10).ToString();
+                stringS = (currentTime.Second % 10).ToString() + (currentTime.Second / 10).ToString();
+
+                _display.DrawRectangle(colorBackground, 1, 0, 0, screenWidth, screenHeight, 0, 0, colorBackground, 0, 0, colorBackground, 0, 0, 255);
+
+                _azmdrawing.DrawStringAligned(_display, colorForeground, font_7barPBdReverse24, stringS + ":" + stringM + ":" + stringH, AZMDrawing.ALIGN_CENTER, 0, AZMDrawing.VALIGN_MIDDLE, 0);
+
+
+            }
+            else
+            {
+
+                _azmdrawing.DrawDigitalClock(_display, Color.White, Color.Black, font_7barPBd24, currentTime, true);
+                ++showDigitalCounter;
+
+                if (showDigitalCounter > SHOW_DIGITAL_SECOND)
+                {
+                    showDigital = false;
+                    showDigitalCounter = 0;
+                }
+
+            }
+
+            _display.Flush();
+
+        }
+
+        private static void Current_OnButtonPress(Buttons button, Microsoft.SPOT.Hardware.InterruptPort port, ButtonDirection direction, DateTime time)
+        {
+
+            if (direction == ButtonDirection.Up)
+            {
+                if (button == Buttons.TopRight)
+                {
+                    if (showDigital == false)
+                    {
+                        --displayMode;
+                        if (displayMode < 0)
+                        {
+                            displayMode = MAX_DISPLAY_MODE;
+                        }
+                    }
+                    else
+                    {
+                        showDigital = false;
+                    }
+                }
+                else if (button == Buttons.MiddleRight)
+                {
+                    if (showDigital != true)
+                    {
+                        showDigital = true;
+                        showDigitalCounter = 0;
+                        UpdateTime(null);
+                    }
+                    else
+                    {
+                        showDigital = false;
+                    }
+                }
+                else if (button == Buttons.BottomRight)
+                {
+                    if (showDigital == false)
+                    {
+                        ++displayMode;
+                        if (displayMode > MAX_DISPLAY_MODE)
+                        {
+                            displayMode = 0;
+                        }
+                    }
+                    else
+                    {
+                        showDigital = false;
+                    }
+                }
+
+                switch (displayMode)
+                {
+
+                    case DISPLAY_MODE_BLACK:
+
+                        colorForeground = Color.White;
+                        colorBackground = Color.Black;
+                        UpdateTime(null);
+
+                        break;
+
+                    case DISPLAY_MODE_WHITE:
+
+                        colorForeground = Color.Black;
+                        colorBackground = Color.White;
+                        UpdateTime(null);
+
+                        break;
+
+                }
+
+            }
+
+        }
+
+    }
+
+}
