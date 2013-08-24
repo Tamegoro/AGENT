f17b643fc423f34965ca6665ac76d99cabad696b
Tamegoro <tamemobile@gmail.com>
2013-08-24 12:44:12 +0900

    Add "Simple (Part 3)".

---
+++ SimplePart03/SimplePart03/SimplePart03.cs
@@ -0,0 +1,270 @@
+﻿using System;
+using Microsoft.SPOT;
+using Microsoft.SPOT.Hardware;
+using Microsoft.SPOT.Presentation;
+using Microsoft.SPOT.Presentation.Media;
+using System.Threading;
+using AGENT.AZMutil.AZMDrawing;
+using Agent.Contrib.Hardware;
+
+
+namespace SimplePart03
+{
+    public class SimplePart03
+    {
+
+        static Bitmap _display;
+
+        static Timer _updateClockTimer;
+        static TimeSpan dueTime;
+        static TimeSpan period;
+
+        static DateTime currentTime;
+
+        static Font font7barPBd24 = Resources.GetFont(Resources.FontResources._7barPBd24);
+
+        static Color colorForeground;
+        static Color colorBackground;
+
+        static AZMDrawing _azmdrawing;
+
+        static int degreeH = 0;
+        static int degreeM = 0;
+        static int degreeS = 0;
+
+        static int screenWidth = 0;
+        static int screenHeight = 0;
+
+        static int screenCenterX = 0;
+        static int screenCenterY = 0;
+
+        static int lengthHand = 0;
+
+        static int displayMode = DISPLAY_MODE_BLACK;
+
+        static bool showSecond = false;
+
+        static bool showDigital = false;
+        static int showDigitalCounter = 0;
+
+        const int SHOW_DIGITAL_SECOND = 10;
+
+        //const int LENGTH_HOUR_HAND = 18;
+        //const int LENGTH_MINUTE_HAND = 18;
+        //const int LENGTH_SECOND_HAND = 18;
+
+        const int THICKNESS_HOUR_HAND = 5;
+        const int THICKNESS_MINUTE_HAND = 4;
+        const int THICKNESS_SECOND_HAND = 2;
+
+        const int MARGIN_DISK_EDGE = 3;
+        const int THICKNESS_DISK = 2;
+
+        const int MAX_DISPLAY_MODE = 3;
+
+        const int DISPLAY_MODE_BLACK = 0;
+        const int DISPLAY_MODE_SECOND_BLACK = 1;
+        const int DISPLAY_MODE_WHITE = 2;
+        const int DISPLAY_MODE_SECOND_WHITE = 3;
+
+
+        public static void Main()
+        {
+
+            _display = new Bitmap(Bitmap.MaxWidth, Bitmap.MaxHeight);
+
+            _azmdrawing = new AZMDrawing();
+
+            screenWidth = _display.Width;
+            screenHeight = _display.Height;
+
+            screenCenterX = screenWidth / 2;
+            screenCenterY = screenHeight / 2;
+
+            lengthHand = (screenCenterX - (MARGIN_DISK_EDGE + THICKNESS_DISK)) / 3 + 1;
+
+            displayMode = DISPLAY_MODE_BLACK;
+            colorForeground = Color.White;
+            colorBackground = Color.Black;
+
+            showDigital = false;
+
+            currentTime = new DateTime();
+            currentTime = DateTime.Now;
+
+            UpdateTime(null);
+
+            currentTime = DateTime.Now;
+
+            dueTime = new TimeSpan(0, 0, 0, 0, 1000 - currentTime.Millisecond);
+            period = new TimeSpan(0, 0, 0, 1, 0);
+
+            _updateClockTimer = new Timer(UpdateTime, null, dueTime, period);
+
+            ButtonHelper.ButtonSetup = new Buttons[]
+            {
+                Buttons.BottomRight, Buttons.MiddleRight, Buttons.TopRight
+            };
+
+            ButtonHelper.Current.OnButtonPress += Current_OnButtonPress;
+
+            Thread.Sleep(Timeout.Infinite);
+
+        }
+
+        static void UpdateTime(object state)
+        {
+
+            currentTime = DateTime.Now;
+
+            _display.Clear();
+
+
+            if (showDigital == false)
+            {
+
+                degreeH = _azmdrawing.HourToAngle(currentTime.Hour, currentTime.Minute);
+                degreeM = _azmdrawing.MinuteToAngle(currentTime.Minute);
+
+                _display.DrawRectangle(colorBackground, 1, 0, 0, screenWidth, screenHeight, 0, 0, colorBackground, 0, 0, colorBackground, 0, 0, 255);
+
+
+                _azmdrawing.DrawCircle(_display, colorForeground, THICKNESS_DISK, screenCenterX, screenCenterY, screenCenterX - MARGIN_DISK_EDGE, screenCenterY - MARGIN_DISK_EDGE);
+
+                _azmdrawing.DrawAngledLine(_display, colorForeground, THICKNESS_HOUR_HAND, degreeH, screenCenterX, screenCenterY, lengthHand * 2, lengthHand);
+                _azmdrawing.DrawAngledLine(_display, colorForeground, THICKNESS_MINUTE_HAND, degreeM, screenCenterX, screenCenterY, lengthHand, lengthHand);
+
+                //_azmdrawing.DrawAngledLine(_display, colorForeground, THICKNESS_HOUR_HAND, degreeH, screenCenterX, screenCenterY, screenCenterX - (MARGIN_DISK_EDGE + LENGTH_HOUR_HAND), LENGTH_HOUR_HAND);
+                //_azmdrawing.DrawAngledLine(_display, colorForeground, THICKNESS_MINUTE_HAND, degreeM, screenCenterX, screenCenterY, screenCenterX - (MARGIN_DISK_EDGE + LENGTH_HOUR_HAND + LENGTH_MINUTE_HAND), LENGTH_MINUTE_HAND);
+
+                if (showSecond == true)
+                {
+
+                    degreeS = _azmdrawing.SecondToAngle(currentTime.Second);
+
+                    _azmdrawing.DrawAngledLine(_display, colorForeground, THICKNESS_SECOND_HAND, degreeS, screenCenterX, screenCenterY, 0, lengthHand);
+
+                    //_azmdrawing.DrawAngledLine(_display, colorForeground, THICKNESS_SECOND_HAND, degreeS, screenCenterX, screenCenterY, screenCenterX - (MARGIN_DISK_EDGE + LENGTH_HOUR_HAND + LENGTH_MINUTE_HAND + LENGTH_SECOND_HAND), LENGTH_SECOND_HAND);
+
+                    _display.DrawEllipse(colorForeground, 1, screenCenterX, screenCenterY, 1, 1, colorForeground, 0, 0, colorForeground, 0, 0, 255);
+
+                }
+
+
+            }
+            else
+            {
+
+                _azmdrawing.DrawDigitalClock(_display, Color.White, Color.Black, font7barPBd24, currentTime, true);
+                ++showDigitalCounter;
+
+                if (showDigitalCounter > SHOW_DIGITAL_SECOND)
+                {
+                    showDigital = false;
+                    showDigitalCounter = 0;
+                }
+
+            }
+
+            _display.Flush();
+
+        }
+
+        private static void Current_OnButtonPress(Buttons button, Microsoft.SPOT.Hardware.InterruptPort port, ButtonDirection direction, DateTime time)
+        {
+
+            if (direction == ButtonDirection.Up)
+            {
+                if (button == Buttons.TopRight)
+                {
+                    if (showDigital == false)
+                    {
+                        --displayMode;
+                        if (displayMode < 0)
+                        {
+                            displayMode = MAX_DISPLAY_MODE;
+                        }
+                    }
+                    else
+                    {
+                        showDigital = false;
+                    }
+                }
+                else if (button == Buttons.MiddleRight)
+                {
+                    if (showDigital != true)
+                    {
+                        showDigital = true;
+                        showDigitalCounter = 0;
+                    }
+                    else
+                    {
+                        showDigital = false;
+                    }
+                }
+                else if (button == Buttons.BottomRight)
+                {
+                    if (showDigital == false)
+                    {
+                        ++displayMode;
+                        if (displayMode > MAX_DISPLAY_MODE)
+                        {
+                            displayMode = 0;
+                        }
+                    }
+                    else
+                    {
+                        showDigital = false;
+                    }
+                }
+
+                switch (displayMode)
+                {
+
+                    case DISPLAY_MODE_BLACK:
+
+                        showSecond = false;
+                        colorForeground = Color.White;
+                        colorBackground = Color.Black;
+                        UpdateTime(null);
+
+                        break;
+
+                    case DISPLAY_MODE_SECOND_BLACK:
+
+                        showSecond = true;
+                        colorForeground = Color.White;
+                        colorBackground = Color.Black;
+                        UpdateTime(null);
+
+                        break;
+
+                    case DISPLAY_MODE_WHITE:
+
+                        showSecond = false;
+                        colorForeground = Color.Black;
+                        colorBackground = Color.White;
+                        UpdateTime(null);
+
+                        break;
+
+                    case DISPLAY_MODE_SECOND_WHITE:
+
+                        showSecond = true;
+                        colorForeground = Color.Black;
+                        colorBackground = Color.White;
+                        UpdateTime(null);
+
+                        break;
+
+                }
+
+            }
+
+        }
+
+
+    }
+
+}
+
