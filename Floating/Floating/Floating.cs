9b29dc4ebb1fa6b740437aa0c98818750658ddfc
Tamegoro <tamemobile@gmail.com>
2013-09-04 21:00:51 +0900

    Add "Floating".

---
+++ Floating/Floating/Floating.cs
@@ -0,0 +1,314 @@
+﻿using System;
+using Microsoft.SPOT;
+using Microsoft.SPOT.Hardware;
+using Microsoft.SPOT.Presentation;
+using Microsoft.SPOT.Presentation.Media;
+using System.Threading;
+using AGENT.AZMutil.AZMDrawing;
+using Agent.Contrib.Hardware;
+
+
+namespace Floating
+{
+    public class Floating
+    {
+
+        static Bitmap _display;
+
+        static Timer _updateClockTimer;
+        static TimeSpan dueTime;
+        static TimeSpan period;
+
+        static DateTime currentTime;
+
+        static Font font7barPBd24 = Resources.GetFont(Resources.FontResources._7barPBd24);
+
+        static Color colorForeground;
+        static Color colorBackground;
+        static Color colorDisk;
+
+        static AZMDrawing _azmdrawing;
+        static AGENT.AZMutil.Point _point;
+
+        static Random _random;
+
+        static int degreeH = 0;
+        static int degreeM = 0;
+        static int degreeS = 0;
+
+        static int screenWidth = 0;
+        static int screenHeight = 0;
+
+        static int screenCenterX = 0;
+        static int screenCenterY = 0;
+
+        static int diskRadius = 0;
+
+        static int randomDegree = 0;
+        
+        static int displayMode = DISPLAY_MODE_BLACK;
+
+        static bool showDigital = false;
+        static int showDigitalCounter = 0;
+
+        const int SHOW_DIGITAL_SECOND = 10;
+        
+        const int MAX_DISPLAY_MODE = 3;
+
+        const int LENGTH_HOUR_HAND_MOD = 13;
+        const int THICKNESS_HOUR_HAND = 4;
+        const int LENGTH_MINUTE_HAND_MOD = 3;
+        const int THICKNESS_MINUTE_HAND = 4;
+        const int LENGTH_SECOND_HAND_MOD = 4;
+        const int THICKNESS_SECOND_HAND = 2;
+
+        const int THIKNESS_RIM = 4;
+        const int DISK_RATIO = 3;
+        const int RADIUS_OUTER_PIN = 2;
+        const int RADIUS_INNER_PIN = 1;
+
+        const int DISPLAY_MODE_BLACK = 0;
+        const int DISPLAY_MODE_BLACK_DISK = 1;
+        const int DISPLAY_MODE_WHITE = 2;
+        const int DISPLAY_MODE_WHITE_DISK = 3;
+
+        const int MOVE_INTERVAL = 1000;
+        const int MOVE_DISTANCE = 2;
+        
+
+        public static void Main()
+        {
+
+            _display = new Bitmap(Bitmap.MaxWidth, Bitmap.MaxHeight);
+
+            _azmdrawing = new AZMDrawing();
+            _point = new AGENT.AZMutil.Point();
+
+            screenWidth = _display.Width;
+            screenHeight = _display.Height;
+
+            screenCenterX = screenWidth / 2;
+            screenCenterY = screenHeight / 2;
+
+            diskRadius = screenWidth / DISK_RATIO;
+
+            displayMode = DISPLAY_MODE_BLACK;
+            colorForeground = Color.White;
+            colorBackground = Color.Black;
+            colorDisk = Color.Black;
+
+            showDigital = false;
+
+            currentTime = new DateTime();
+            currentTime = DateTime.Now;
+
+            _random = new Random(currentTime.Millisecond);
+
+            _point.X = screenCenterX;
+            _point.Y = screenCenterY;
+
+            randomDegree = 0;
+
+            UpdateTime(null);
+
+            currentTime = DateTime.Now;
+
+            dueTime = new TimeSpan(0, 0, 0, 0, 1000 - currentTime.Millisecond);
+            period = new TimeSpan(0, 0, 0, 0, MOVE_INTERVAL);
+
+            _updateClockTimer = new Timer(UpdateTime, null, dueTime, period);
+
+            ButtonHelper.ButtonSetup = new Buttons[]
+            {
+                Buttons.BottomRight, Buttons.MiddleRight, Buttons.TopRight
+            };
+
+            ButtonHelper.Current.OnButtonPress += Current_OnButtonPress;
+
+            Thread.Sleep(Timeout.Infinite);
+
+        }
+
+        static void UpdateTime(object state)
+        {
+
+            currentTime = DateTime.Now;
+
+            if (showDigital == false)
+            {
+
+                degreeH = _azmdrawing.HourToAngle(currentTime.Hour, currentTime.Minute);
+                degreeM = _azmdrawing.MinuteToAngle(currentTime.Minute);
+                degreeS = _azmdrawing.SecondToAngle(currentTime.Second);
+
+                _display.Clear();
+
+                _display.DrawRectangle(colorBackground, 1, 0, 0, screenWidth, screenHeight, 0, 0, colorBackground, 0, 0, colorBackground, 0, 0, 255);
+
+                if (displayMode == DISPLAY_MODE_BLACK_DISK || displayMode == DISPLAY_MODE_WHITE_DISK)
+                {
+                    _display.DrawEllipse(colorDisk, 1, _point.X, _point.Y, diskRadius, diskRadius, colorDisk, 0, 0, colorDisk, 0, 0, 255);
+                }
+                else if (displayMode == DISPLAY_MODE_BLACK || displayMode == DISPLAY_MODE_WHITE)
+                {
+                    _display.DrawEllipse(colorForeground, 1, _point.X, _point.Y, diskRadius, diskRadius, colorForeground, 0, 0, colorForeground, 0, 0, 255);
+                    _display.DrawEllipse(colorBackground, 1, _point.X, _point.Y, diskRadius - THIKNESS_RIM, diskRadius - THIKNESS_RIM, colorBackground, 0, 0, colorBackground, 0, 0, 255);
+                }
+
+                _azmdrawing.DrawAngledLine(_display, colorForeground, THICKNESS_HOUR_HAND, degreeH, _point.X, _point.Y, 0, diskRadius - (THIKNESS_RIM + LENGTH_HOUR_HAND_MOD));
+                _azmdrawing.DrawAngledLine(_display, colorForeground, THICKNESS_MINUTE_HAND, degreeM, _point.X, _point.Y, 0, diskRadius - (THIKNESS_RIM + LENGTH_MINUTE_HAND_MOD));
+                _azmdrawing.DrawAngledLine(_display, colorForeground, THICKNESS_SECOND_HAND, degreeS, _point.X, _point.Y, 0, diskRadius - (THIKNESS_RIM + LENGTH_SECOND_HAND_MOD));
+
+                _display.DrawEllipse(colorForeground, 1, _point.X, _point.Y, RADIUS_OUTER_PIN, RADIUS_OUTER_PIN, colorForeground, 0, 0, colorForeground, 0, 0, 255);
+
+                if (displayMode == DISPLAY_MODE_BLACK_DISK || displayMode == DISPLAY_MODE_WHITE_DISK)
+                {
+                    _display.DrawEllipse(colorDisk, 1, _point.X, _point.Y, RADIUS_INNER_PIN, RADIUS_INNER_PIN, colorDisk, 0, 0, colorDisk, 0, 0, 255);
+                }
+                else if (displayMode == DISPLAY_MODE_BLACK || displayMode == DISPLAY_MODE_WHITE)
+                {
+                    _display.DrawEllipse(colorBackground, 1, _point.X, _point.Y, RADIUS_INNER_PIN, RADIUS_INNER_PIN, colorBackground, 0, 0, colorBackground, 0, 0, 255);
+                }
+
+
+                randomDegree = _random.Next(360);
+                _point = _azmdrawing.FindPointDegreeDistance(randomDegree, _point.X, _point.Y, MOVE_DISTANCE);
+
+                if (_point.X - diskRadius < 0)
+                {
+                    _point.X = diskRadius;
+                }
+
+                if (_point.Y - diskRadius < 0)
+                {
+                    _point.Y = diskRadius;
+                }
+
+                if (screenWidth - 1 < _point.X + diskRadius)
+                {
+                    _point.X = screenWidth - 1 - diskRadius;
+                }
+
+                if (screenHeight - 1 < _point.Y + diskRadius)
+                {
+                    _point.Y = screenHeight - 1 - diskRadius;
+                }
+
+            }
+            else
+            {
+
+                _azmdrawing.DrawDigitalClock(_display, Color.White, Color.Black, font7barPBd24, currentTime, true);
+                ++showDigitalCounter;
+
+                if (showDigitalCounter > SHOW_DIGITAL_SECOND)
+                {
+                    showDigital = false;
+                    showDigitalCounter = 0;
+                }
+
+            }
+
+            _display.Flush();
+
+        }
+
+        private static void Current_OnButtonPress(Buttons button, Microsoft.SPOT.Hardware.InterruptPort port, ButtonDirection direction, DateTime time)
+        {
+
+            if (direction == ButtonDirection.Up)
+            {
+                if (button == Buttons.TopRight)
+                {
+                    if (showDigital == false)
+                    {
+                        --displayMode;
+                        if (displayMode < 0)
+                        {
+                            displayMode = MAX_DISPLAY_MODE;
+                        }
+                    }
+                    else
+                    {
+                        showDigital = false;
+                    }
+                }
+                else if (button == Buttons.MiddleRight)
+                {
+                    if (showDigital != true)
+                    {
+                        showDigital = true;
+                        showDigitalCounter = 0;
+                    }
+                    else
+                    {
+                        showDigital = false;
+                    }
+                }
+                else if (button == Buttons.BottomRight)
+                {
+                    if (showDigital == false)
+                    {
+                        ++displayMode;
+                        if (displayMode > MAX_DISPLAY_MODE)
+                        {
+                            displayMode = 0;
+                        }
+                    }
+                    else
+                    {
+                        showDigital = false;
+                    }
+                }
+
+                switch (displayMode)
+                {
+
+
+                    case DISPLAY_MODE_BLACK:
+
+                        colorForeground = Color.White;
+                        colorBackground = Color.Black;
+                        colorDisk = Color.Black;
+                        UpdateTime(null);
+
+                        break;
+
+                    case DISPLAY_MODE_BLACK_DISK:
+
+                        colorForeground = Color.Black;
+                        colorBackground = Color.Black;
+                        colorDisk = Color.White;
+                        UpdateTime(null);
+
+                        break;
+
+                    case DISPLAY_MODE_WHITE:
+
+                        colorForeground = Color.Black;
+                        colorBackground = Color.White;
+                        colorDisk = Color.White;
+                        UpdateTime(null);
+
+                        break;
+
+                    case DISPLAY_MODE_WHITE_DISK:
+
+                        colorForeground = Color.White;
+                        colorBackground = Color.White;
+                        colorDisk = Color.Black;
+                        UpdateTime(null);
+
+                        break;
+
+                }
+
+            }
+
+        }
+
+
+    }
+
+}
+
